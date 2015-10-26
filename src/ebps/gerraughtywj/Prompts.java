5b24a4d6219c511d0e5ea2bc39dc7c0643077a4e
zXTechnoWolf <zxtechnowolf@gmail.com>
2015-10-26 11:34:48 -0400

    Initial Commit

---
+++ src/ebps/gerraughtywj/Prompts.java
@@ -0,0 +1,21 @@
+package ebps.gerraughtywj;
+
+public class Prompts {
+	
+	public static final String doorwayPrompt = "You are in a creepy house! Would you like to go upstairs or into the kitchen?";
+	public static final String kitchenPrompt = "There is a long countertop with dirty dishes everywhere. Off to one side there is, as you'd expect, a refridgerator. You may open the refridgerator or look in a cabinet";
+	public static final String upstairsPrompt = "Upstairs you see a hallway. At the end of the hallway is the master bedroom. There is also a bathroom off the hallway. Where would you like to go?";
+	public static final String fridgePrompt = "Inside the refridgerator you see food and stuff. It looks pretty nasty. Would you like to eat some of the food?";
+	public static final String cabinetPrompt = "You make the prompt!";
+	public static final String bedroomPrompt = "You are in a plush bedroom, with expensive-looking hardwood furniture. The bed is unmade. In the back of the room, the closet door is ajar. Would you like to open the door?";
+	public static final String bathroomPrompt = "You make the prompt!";
+	public static final String fridgeYes = "You make the prompt!";
+	public static final String fridgeNo = "You make the prompt!";
+	public static final String cabinetYes = "You make the prompt!";
+	public static final String cabinetNo = "You make the prompt!";
+	public static final String bedroomYes = "You make the prompt!";
+	public static final String bedroomNo = "You make the prompt!";
+	public static final String bathroomYes = "You make the prompt!";
+	public static final String bathroomNo = "You make the prompt!";
+
+}
