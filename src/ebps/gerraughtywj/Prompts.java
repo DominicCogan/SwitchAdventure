fee8f5a58a45b2b3999b7c288de43255eccfc890
zXTechnoWolf <zxtechnowolf@gmail.com>
2015-10-27 12:17:32 -0400

    Still misspelled refrigerator

--- src/ebps/gerraughtywj/Prompts.java
+++ src/ebps/gerraughtywj/Prompts.java
@@ -3,7 +3,7 @@ package ebps.gerraughtywj;
 public class Prompts {
 	
 	public static final String doorwayPrompt = "You are in a creepy house! Would you like to go upstairs or into the kitchen?";
-	public static final String kitchenPrompt = "There is a long countertop with dirty dishes everywhere. Off to one side there is, as you'd expect, a refrigerator. You may open the refridgerator or look in a cabinet";
+	public static final String kitchenPrompt = "There is a long countertop with dirty dishes everywhere. Off to one side there is, as you'd expect, a refrigerator. You may open the refrigerator or look in a cabinet";
 	public static final String upstairsPrompt = "Upstairs you see a hallway. At the end of the hallway is the master bedroom. There is also a bathroom off the hallway. Where would you like to go?";
 	public static final String fridgePrompt = "Inside the refrigerator you see food and stuff. It looks pretty nasty. Would you like to eat some of the food?";
 	public static final String cabinetPrompt = "You make the prompt!";
